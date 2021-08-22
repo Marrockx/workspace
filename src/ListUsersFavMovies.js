bfb11bdb05b8c221ee6f0b24c7ea43731fc0c6d3
Mariam Adekola <mariam.adekola10@gmail.com>
2021-08-22 13:16:46 +0100

    code-solution

---
+++ src/ListUsersFavMovies.js
@@ -0,0 +1,34 @@
+/** @format */
+
+import React, { Component } from "react";
+
+class ListUsersFavMovies extends Component {
+	render() {
+		return (
+			<div>
+				<ol className='movies-list'>
+					{this.props.profiles.map(
+						(profile, userName, favoriteMovie) => (
+							[
+								(userName = this.props.users[profile.userID].name),
+								(favoriteMovie =
+									this.props.movies[profile.favoriteMovieID].name),
+							],
+							(
+								<li key={profile.id}>
+									<div>
+										<p>
+											{userName}'s favorite movie is {favoriteMovie}
+										</p>
+									</div>
+								</li>
+							)
+						)
+					)}
+				</ol>
+			</div>
+		);
+	}
+}
+
+export default ListUsersFavMovies;
